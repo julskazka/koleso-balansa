d1464532ddebb4a5039fea913b711b762c3c19dd
Julskazka <gogina.jul@gmail.com>
2026-09-16 01:21:50 +0300

    Point reflection loader to new Notibot form

--- wheel/reflection-form-v24.js
+++ wheel/reflection-form-v24.js
@@ -1,10 +1,10 @@
 (() => {
   'use strict';
-  const FORM_ID = '6OilaTGVmsR7PrL9f6h2ni';
+  const FORM_ID = '69cGsZT0FiHaSjDptu1xFo';
   window.WHEEL_NOTIBOT_CONFIG = Object.assign({}, window.WHEEL_NOTIBOT_CONFIG || {}, { reflectionFormId: FORM_ID });
   if (document.querySelector('script[data-reflection-v25]')) return;
   const script = document.createElement('script');
-  script.src = './reflection-form-v25.js?v=32';
+  script.src = './reflection-form-v25.js?v=33';
   script.dataset.reflectionV25 = 'true';
   script.async = false;
   document.head.appendChild(script);
