97378183d5b16bcb9128a125f32b5d14b20fe0d0
Julskazka <gogina.jul@gmail.com>
2026-09-15 22:17:13 +0300

    Add reflection form v25 placeholder

---
+++ wheel/reflection-form-v25.js
@@ -0,0 +1,5 @@
+(() => {
+  'use strict';
+  const FORM_ID = '6OilaTGVmsR7PrL9f6h2ni';
+  window.WHEEL_NOTIBOT_CONFIG = Object.assign({}, window.WHEEL_NOTIBOT_CONFIG || {}, { reflectionFormId: FORM_ID });
+})();
